7bc15589662a8f100e8e4eb7e1cd4c985156f6c1
w7436 <3239741254@qq.com>
2020-01-01 22:43:14 +0800

    代码上传

--- 顺序表/顺序表/List.c
+++ 顺序表/顺序表/List.c
@@ -3,70 +3,96 @@
 
 
 //初始化顺序表
-void SqListInit(SqList *p) {
-	assert(p!=NULL);
-	p->array = (int *)malloc(sizeof(int)*p->capacity);
-	p->capacity = 10;
-	p->size = 0;
-}
 
-//销毁顺序表
-void SqListdestroy(SqList *p) {
-	assert(p != NULL);
-	assert(p->capacity != NULL);
-	free(p->capacity);
+void SeqListInit(SqList *ps)
+{
+	//多多使用assert断言，以防程序出现不可测得问题
+	assert(ps != NULL);
+	//将顺序表中的数组进行赋值
+	memset(ps->data, 0x00, MAXSIZE * sizeof(int));
+	// 顺序表中的有效元素初始为0
+		ps->size = 0;
 }
+	
+void SeqListPrint(SeqList *ps)
+{
+	assert(ps != NULL);
+	//使用for循环依次打印所有的有效元素的个数
+	for (int i = 0; i < ps->size; i++)
+	{
+		printf("%d->", ps->data[i]);
+	}
+	printf("NULL\n");
+
+
+
+
+////初始化顺序表
+//void SqListInit(SqList *p) {
+//	assert(p!=NULL);
+//	p->array = (int *)malloc(sizeof(int)*p->capacity);
+//	p->capacity = 10;
+//	p->size = 0;
+//}
+//
+////销毁顺序表
+//void SqListdestroy(SqList *p) {
+//	assert(p != NULL);
+//	assert(p->capacity != NULL);
+//	free(p->capacity);
+//}
+//
 
 //尾插
-void SqListPushBack(SqList *p, int data) {
-	assert(p!=NULL);
-	assert(p->array!=NULL);
-
-	p->array[p->size] = data;
-	p->size++;
-}
-//头插
-void SqListPopPush(SqList *p, int data) {
-	assert(p != NULL);
-	assert(p->array != NULL);
-
-	for (int i = p->size; i >= 1;i--) {
-		p->array[i] = p->array[i-1];
-	}
-	p->array[0] = data;
-	p->size++;
-}
-
-//头删
-void SqListPopFront(SqList *p) {
-	assert(p != NULL);
-	assert(p->array != NULL);
-
-	for (int i = 0; i < p->size;i++) {
-		p->array[i] = p->array[i + 1];
-	}
-	p->size--;
-}
-//尾删
-void SqListPopBack(SqList *p) {
-	assert(p != NULL);
-	assert(p->array != NULL);
-
-	p->size--;
-
-}
-//任意位置的插入
-void SeqListInsert(SqList *p, int pos, int data) {
-	assert(p != NULL);
-	assert(p->array != NULL);
-
-	if (pos>p->capacity) {
-		return;
-	}
-	for (int i = pos ; i <= p->size;i++) {
-		p->array[i] = p->array[i-1];
-	}
-	p->array[pos - 1] = data;
-	p->size++;
-
-}
+//void SqListPushBack(SqList *p, int data) {
+//	assert(p!=NULL);
+//	assert(p->array!=NULL);
+//
+//	p->array[p->size] = data;
+//	p->size++;
+//}
+////头插
+//void SqListPopPush(SqList *p, int data) {
+//	assert(p != NULL);
+//	assert(p->array != NULL);
+//
+//	for (int i = p->size; i >= 1;i--) {
+//		p->array[i] = p->array[i-1];
+//	}
+//	p->array[0] = data;
+//	p->size++;
+//}
+//
+////头删
+//void SqListPopFront(SqList *p) {
+//	assert(p != NULL);
+//	assert(p->array != NULL);
+//
+//	for (int i = 0; i < p->size;i++) {
+//		p->array[i] = p->array[i + 1];
+//	}
+//	p->size--;
+//}
+////尾删
+//void SqListPopBack(SqList *p) {
+//	assert(p != NULL);
+//	assert(p->array != NULL);
+//
+//	p->size--;
+//
+//}
+////任意位置的插入
+//void SeqListInsert(SqList *p, int pos, int data) {
+//	assert(p != NULL);
+//	assert(p->array != NULL);
+//
+//	if (pos>p->capacity) {
+//		return;
+//	}
+//	for (int i = pos ; i <= p->size;i++) {
+//		p->array[i] = p->array[i-1];
+//	}
+//	p->array[pos - 1] = data;
+//	p->size++;
+//
+//}
--- 顺序表/顺序表/Sqlist.h
+++ 顺序表/顺序表/Sqlist.h
@@ -4,20 +4,20 @@
 //线性表：是N个具有相同特性的数据元素的有限序列
 
 
-////静态
-//typedef struct {
-//	int data[MAXSIZE];
-//	int length;
-//}SqList;
-
-//动态
+//静态
 typedef struct {
-	int *array;
-	int capacity;//总容量
+	int data[MAXSIZE];
 	int size;
 }SqList;
 
+////动态
+//typedef struct {
+//	int *array;
+//	int capacity;//总容量
+//	int size;
+//}SqList;
+//
 
-void SqListInit(SqList *p);
-void SqListdestroy(SqList *P);
-void SqListPushBack(SqList *p, int data);
+//void SqListInit(SqList *p);
+//void SqListdestroy(SqList *P);
+//void SqListPushBack(SqList *p, int data);
